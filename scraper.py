92069c1e314defd3551a72a8eadb1083d64f69b3
Tyler Huynh <huynht9@uci.edu>
2025-04-24 17:30:59 -0700

    Began implementing scrapper.py extract_next_links() method ensuring valid response status

--- scraper.py
+++ scraper.py
@@ -15,7 +15,11 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    if resp.status != 200:
+        print("Error occurred while extracting links:", resp.error)
+        return list()
+    links = []
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
